4eff2690679023ca314cbc03c035acdaa796e0f0
Matthew Brecknell <matthew@brecknell.net>
2021-02-12 09:41:30 +1100

    log version information

--- build.sh
+++ build.sh
@@ -26,9 +26,18 @@ trace() {
 
 trace "pulling alpine and $registry/caddy-base"
 
-docker pull --quiet alpine
+get_alpine_version() {
+  local image="$1"
+  docker run --rm -i "$image" sh -c 'source /etc/os-release && echo $VERSION_ID'
+}
 
-if ! docker pull --quiet $registry/caddy-base; then
+docker pull --quiet alpine
+alpine_version="$(get_alpine_version alpine)"
+trace "Alpine Linux version $alpine_version"
+
+if docker pull --quiet $registry/caddy-base; then
+  old_alpine_version="$(get_alpine_version $registry/caddy-base)"
+else
   trace "$registry/caddy-base not found"
   first_build_base=true
 fi
@@ -59,13 +68,13 @@ test_apk_updates() {
      && (apk list --installed | sort > /tmp/1) \
      && apk upgrade \
      && (apk list --installed | sort > /tmp/2) \
-     && diff /tmp/1 /tmp/2'
+     && diff -U 0 -L "old packages" /tmp/1 -L "new packages" /tmp/2'
 }
 
 if ! $first_build_base; then
   trace "testing whether there is a new alpine image"
   if fresh_base_layers alpine $registry/caddy-base; then
-    trace "there is a new alpine image"
+    trace "there is a new alpine image (old=$old_alpine_version, new=$alpine_version)"
     rebuild_base=true
   elif ! test_apk_updates; then
     trace "there is not a new alpine image, but there are apk updates"
@@ -90,8 +99,10 @@ trace "unconditionally building a new caddy executable"
 
 docker build --no-cache --pull --tag $registry/caddy-exe dockerfiles/caddy-exe
 new_caddy_exe_container_id=$(docker create $registry/caddy-exe /caddy)
-docker cp $new_caddy_exe_container_id:/caddy dockerfiles/caddy/caddy
+docker cp $new_caddy_exe_container_id:/go/bin/caddy dockerfiles/caddy/caddy
 docker rm $new_caddy_exe_container_id
+golang_version="$(docker run --rm -i $registry/caddy-exe go version | awk '{print $3}')"
+caddy_version="$(docker run --rm -i $registry/caddy-exe caddy version | awk '{print $1}')"
 
 if ! $first_build_base && ! $rebuild_base && ! $rebuild_apk; then
   trace "pulling $registry/caddy to compare caddy executables"
@@ -104,11 +115,16 @@ if ! $first_build_base && ! $rebuild_base && ! $rebuild_apk; then
   else
     trace "comparing old and new caddy executables"
     old_caddy_exe_container_id=$(docker create $registry/caddy)
-    docker cp $old_caddy_exe_container_id:/bin/caddy dockerfiles/caddy/caddy-$old_caddy_exe_container_id
+    old_caddy_exe=dockerfiles/caddy/caddy-$old_caddy_exe_container_id
+    docker cp $old_caddy_exe_container_id:/bin/caddy $old_caddy_exe
     docker rm $old_caddy_exe_container_id
+    old_golang_version=$(docker run --rm -i -v $PWD/$old_caddy_exe:/caddy $registry/caddy-exe go version /caddy | awk '{print $2}')
+    old_caddy_version=$(docker run --rm -i -v $PWD/$old_caddy_exe:/caddy $registry/caddy-exe /caddy version | awk '{print $1}')
 
     if ! cmp dockerfiles/caddy/caddy-$old_caddy_exe_container_id dockerfiles/caddy/caddy; then
       trace "old and new caddy executables differ"
+      trace "Go: $old_golang_version -> $golang_version"
+      trace "Caddy: $old_caddy_version -> $caddy_version"
       rebuild_exe=true
     fi
 
@@ -117,6 +133,13 @@ if ! $first_build_base && ! $rebuild_base && ! $rebuild_apk; then
 fi
 
 # If needed, (re)build and push the final caddy image.
+
+show_versions() {
+  echo "Versions:"
+  echo "  Alpine Linux: $alpine_version"
+  echo "  Go: $golang_version"
+  echo "  Caddy: $caddy_version"
+}
 
 if $first_build_base || $first_build_caddy || $rebuild_base || $rebuild_apk || $rebuild_caddy || $rebuild_exe; then
   trace "building and pushing $registry/caddy"
@@ -128,6 +151,7 @@ if $first_build_base || $first_build_caddy || $rebuild_base || $rebuild_apk || $
     dockerfiles/caddy
   docker push $registry/caddy
 
+  show_versions
   echo "The caddy image was (re)built because:"
   if $first_build_base; then echo "- There wasn't an existing caddy-base image"; fi
   if $first_build_caddy; then echo "- There wasn't an existing caddy image"; fi
@@ -136,6 +160,7 @@ if $first_build_base || $first_build_caddy || $rebuild_base || $rebuild_apk || $
   if $rebuild_caddy; then echo "- The caddy image was not up-to-date with caddy-base"; fi
   if $rebuild_exe; then echo "- The caddy executable has been updated"; fi
 else
+  show_versions
   echo "The caddy image was already up to date."
 fi
 
